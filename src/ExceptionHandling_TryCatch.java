e740d9d3de955502787865408ef4b7481dc7b764
swathyranganathan <swathyranganathan2003@gmail.com>
2024-09-01 18:10:15 +0530

    modified exceptionhandling

--- src/ExceptionHandling_TryCatch.java
+++ src/ExceptionHandling_TryCatch.java
@@ -1,22 +1,54 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class ExceptionHandling_TryCatch {
     public static void main(String[] args) {
-        int i=0;
-        int j=56;
-        try {
-            int k=j/i;
+        Scanner scan=new Scanner(System.in);
+        System.out.println("enter something:");
+
+        try{
+            int a=scan.nextInt();
         }
         catch (Exception e){
-            System.out.println("Something went wrong  " + e);
+            System.out.println(e);
+            System.out.println("something went wrong!!");
+            scan.next();
         }
-        try {
-            int k=j/2;
-            System.out.println(k);
-        }
-        catch (Exception ob){
-            System.out.println("Something went wrong");
-        }
-        System.out.println(j/2);
+        System.out.println("bye");
 
-        System.out.println("  Hi hello Byee");
+        System.out.println("enter a number:");
+
+
+        try{
+            int i = scan.nextInt();
+            int sum= 10/i;
+        }
+        catch (Exception oj){
+            System.out.println(oj);
+            System.out.println("something went wromg!");
+        }
+        System.out.println(" bye bye");
+
+       int m=0;
+       int n=0;
+       int o=0;
+        try {
+            System.out.println("enter a number:");
+             m=scan.nextInt();
+            System.out.println("enter another number:");
+             n=scan.nextInt();
+            o=m/n;
+        }catch (ArithmeticException e){
+            System.out.println(e);
+            System.out.println("invalid input");
+        }
+        catch (InputMismatchException e){
+            System.out.println(e);
+            System.out.println("invalid input");
+        }
+        //System.out.println("the answer:"+o);
+        if(o!=0){
+            System.out.println("the answer:"+o);
+        }
     }
 }
